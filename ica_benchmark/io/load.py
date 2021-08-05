5e0c8ee2486209bde43e832f07cde5ca24d1d3c8
Kotzly <paulo.augusto.viana@hotmail.com>
2021-08-05 00:22:26 -0300

    #8 new class for dataset handling

--- ica_benchmark/io/load.py
+++ ica_benchmark/io/load.py
@@ -1,9 +1,103 @@
 from mne.io import read_raw_gdf
-from pathlib import Path
-import numpy as np
-from ica_benchmark.processing.label import get_annotations_from_gdf
+from mne import Epochs, events_from_annotations, concatenate_raws, concatenate_epochs
 
 PRELOAD = False
+
+class class_or_instancemethod(classmethod):
+    def __get__(self, instance, type_):
+        descr_get = super().__get__ if instance is None else self.__func__.__get__
+        return descr_get(instance, type_)
+
+class BCI_IV_Comp_Dataset():
+
+    EVENT_MAP_DICT = {
+        "769": 0,
+        "770": 1,
+        "771": 2,
+        "772": 3
+    }
+
+    REJECT_MAGNITUDE = 1e-4
+
+    SUBJECT_INFO_KEYS = [
+        "id",
+        "smoking",
+        "alcohol_abuse",
+        "drug_abuse",
+        "medication",
+        "weight",
+        "height",
+        "sex",
+        "handedness",
+        "age",
+    ]
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def load_as_raw(cls, filepath, preload=False):
+        eog_channels = ["EOG-left", "EOG-central", "EOG-right"]
+        gdf_obj = read_raw_gdf(
+            filepath,
+            preload=preload,
+            eog=eog_channels,
+            exclude=eog_channels,
+        )
+    
+        return gdf_obj
+    
+
+    @classmethod
+    def load_as_epochs(cls, filepath, tmin=-.3, tmax=.7, reject=None):
+        if reject is None:
+            reject = cls.REJECT_MAGNITUDE
+
+        gdf_obj = cls.load_as_raw(filepath, preload=True)
+        events, _ = events_from_annotations(gdf_obj, event_id=cls.EVENT_MAP_DICT)
+        epochs = Epochs(
+            gdf_obj,
+            events,
+            event_repeated="drop",
+            reject_by_annotation=True,
+            tmin=tmin,
+            tmax=tmax,
+            reject=dict(eeg=reject)
+        )
+        return epochs
+
+    @classmethod
+    def _parse_info(cls, info_dict):
+
+        parsed_info = {k: v for k, v in info_dict.items() if k in cls.SUBJECT_INFO_KEYS}
+        return parsed_info
+
+    @classmethod
+    def read_metadata_from_raw(cls, raw):
+        return cls._parse_info(
+            raw._raw_extras[0]["subject_info"]
+        )
+
+    @classmethod
+    def read_metadata(cls, filepath):
+        gdf_obj = cls.load_as_raw(filepath, preload=False)
+        return cls.read_metadata_from_raw(gdf_obj)
+
+    @classmethod
+    def load_dataset(cls, filepaths, as_epochs=False, concatenate=False, drop_bad=False, **load_kwargs):
+        objs = list()
+        load_fn = cls.load_as_epochs if as_epochs else cls.load_as_raw
+        concatenate_fn = concatenate_epochs if as_epochs else concatenate_raws
+
+        objs = list()
+        for filepath in filepaths:
+            obj = load_fn(filepath, **load_kwargs)
+            objs.append(obj)
+        
+        if concatenate:
+            objs = concatenate_fn(objs)
+        
+        return objs
 
 
 def join_gdfs_to_numpy(gdfs):
@@ -39,23 +133,6 @@ def load_gdf_file(filepath):
     labels = get_annotations_from_gdf(gdf_data)
 
     return gdf_data, labels, ch_names, info
-
-
-def parse_info(info_dict):
-    cols = [
-        "id",
-        "smoking",
-        "alcohol_abuse",
-        "drug_abuse",
-        "medication",
-        "weight",
-        "height",
-        "sex",
-        "handedness",
-        "age",
-    ]
-    parsed_info = {k: v for k, v in info_dict.items() if k in cols}
-    return parsed_info
 
 
 def load_subject_data(root, subject, filepaths=None, return_as_gdf=True):
