8a56c41dfbdf6d2ad01d7863bf3ff7b68f02aa0f
Kotzly <paulo.augusto.viana@hotmail.com>
2023-05-07 21:59:56 -0300

    #32 fixed experiment, running but not verified

--- experiments/issue_32/experiment.py
+++ experiments/issue_32/experiment.py
@@ -1,11 +1,13 @@
 import numpy as np
 from pathlib import Path
 import time
+import os
 
 from sklearn.metrics import balanced_accuracy_score, cohen_kappa_score, accuracy_score
 from sklearn.pipeline import make_pipeline
 from ica_benchmark.processing.ica import get_ica_instance
 from ica_benchmark.processing.orica_code import ORICA
+from ica_benchmark.io.load import BCI_IV_Comp_Dataset
 
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -18,11 +20,11 @@ from sklearn.feature_selection import SequentialFeatureSelector
 import random
 
 from utils import get_classifier, load_subject_epochs, PSD, ConcatenateChannelsPSD
-from utils import alg_rename, extract_subject_id
+from utils import alg_rename
 
 from colorama import Fore, Style
 from itertools import cycle
-
+import pickle
 
 COLORS = [
     Fore.BLUE,
@@ -32,7 +34,7 @@ COLORS = [
     Fore.RED,
     Fore.BLACK,
     Fore.WHITE,
-    Fore.YELLOW,    
+    Fore.YELLOW,
     Fore.LIGHTBLUE_EX,
     Fore.LIGHTCYAN_EX,
     Fore.LIGHTGREEN_EX,
@@ -45,10 +47,9 @@ COLORS = [
 
 
 def is_ica_stochastic(ica_method):
-    return not(
-        ("orica" in ica_method) or
-        (ica_method in ("sobi", "jade", "none", None, "pca", "whitening"))
-    )
+    is_orica = "orica" in ica_method
+    is_det_prep = ica_method in ("sobi", "jade", "none", None, "pca", "whitening")
+    return not (is_orica or is_det_prep)
 
 
 def is_stochastic(ica_method, clf_method):
@@ -67,7 +68,9 @@ def repeat_deterministic(results_df, times=1):
     non_repeated = original_df.groupby(keys, as_index=False).nunique().query("run == 1")
     for _, (alg, clf) in non_repeated[keys].iterrows():
         for i in range(times):
-            sliced_df = original_df.query("(algorithm == @alg) & (classifier == @clf)").copy()
+            sliced_df = original_df.query(
+                "(algorithm == @alg) & (classifier == @clf)"
+            ).copy()
             if i == sliced_df.run.unique().item():
                 continue
             sliced_df.loc[:, ["run"]] = i
@@ -76,19 +79,21 @@ def repeat_deterministic(results_df, times=1):
     return results_df.reset_index(drop=True)
 
 
-def get_PSD(data, sfreq=250, len_size=250):
+def get_PSD(data, channels, sfreq=250, len_size=250):
     if isinstance(data, Epochs):
         psd = PSD(
+            channels=channels,
             picks=data.ch_names,
             n_fft=1 * len_size,
             n_overlap=len_size // 4,
             n_per_seg=1 * len_size,
             average="mean",
             window="hamming",
-            proj=False
+            proj=False,
         )
     elif isinstance(data, np.ndarray):
         psd = PSD(
+            channels=channels,
             sfreq=sfreq,
             n_fft=1 * len_size,
             n_overlap=len_size // 4,
@@ -101,35 +106,53 @@ def get_PSD(data, sfreq=250, len_size=250):
     return psd
 
 
-def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, random_state=1):
+def save_artifact(artifact, name, save_folder, uid, ica_method, clf_method, n_run):
+
+    save_folder = save_folder / f"{uid}"
+
+    for subfolder in (ica_method, clf_method):
+        if subfolder is not None:
+            save_folder = save_folder / subfolder
+
+    save_folder.mkdir(parents=True, exist_ok=True)
+
+    artifact_path = save_folder / f"{name}_{n_run}.pkl"
+    
+    with open(artifact_path, "wb") as f:
+        pickle.dump(artifact, f)
+
+
+def run(
+    dataset,
+    uid,
+    save_folder,
+    ica_methods=None,
+    clf_methods=None,
+    channels=None,
+    n_runs=10,
+    random_state=1,
+):
     np.random.seed(random_state)
     random.seed(random_state)
 
     ica_method_color_dict = {
-        ica_method: color
-        for ica_method, color
-        in zip(ica_methods, cycle(COLORS))
+        ica_method: color for ica_method, color in zip(ica_methods, cycle(COLORS))
     }
     clf_method_color_dict = {
-        clf_method: color
-        for clf_method, color
-        in zip(clf_methods, cycle(COLORS))
+        clf_method: color for clf_method, color in zip(clf_methods, cycle(COLORS))
     }
 
     ica_methods = ica_methods or [None]
     clf_methods = clf_methods or ["lda"]
 
-    subject_number = int(filepath.name[1:3])
-    (train_epochs, train_labels), (test_epochs, test_labels) = load_subject_epochs(filepath.parent, subject_number)
+    load_kwargs = dict(reject=False, tmin=0.0, tmax=4.0)
+    (train_epochs, train_labels), (test_epochs, test_labels) = load_subject_epochs(
+        dataset, uid, load_kwargs=load_kwargs
+    )
 
-    selected_channels = channels or train_epochs.ch_names if channels is None else channels
-    n_channels = len(selected_channels)
-
-    train_epochs.pick(selected_channels)
-    test_epochs.pick(selected_channels)
+    n_channels = len(train_epochs.ch_names)
 
     results = list()
-    print("[{}]".format(filepath.name))
 
     len_size = 250
 
@@ -160,18 +183,19 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
                         block_update=True,
                         size_block=8,
                         stride=8,
-                        lm_0=.995,
-                        lw_0=.995,
-                        gamma=.6,
+                        lm_0=0.995,
+                        lw_0=0.995,
+                        gamma=0.6,
                         n_sub=n_sub,
                     )
+
                     ICA.fit(x_train)
+                    save_artifact(ICA, "ica", save_folder, uid, ica_method, None, n_run)
 
                     n_epochs, n_channels, n_times = x_train.shape
                     x = x_train.transpose(1, 0, 2).reshape(n_channels, -1)
                     x_train = (
-                        ICA
-                        .transform(
+                        ICA.transform(
                             x,
                             scaling=1e6,
                             save=False,
@@ -188,27 +212,21 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
                     n_epochs, n_channels, n_times = x_test.shape
                     x = x_test.transpose(1, 0, 2).reshape(n_channels, -1)
                     x_test = (
-                        ICA
-                        .transform(
-                            x,
-                            scaling=1e6,
-                            save=False,
-                            warm_start=True
-                        )
+                        ICA.transform(x, scaling=1e6, save=False, warm_start=True)
                         .reshape(n_channels, n_epochs, n_times)
                         .transpose(1, 0, 2)
                     )
+                    save_artifact(ICA, "ica_end", save_folder, uid, ica_method, None, n_run)
 
                 else:
                     ICA = get_ica_instance(ica_method, random_state=run_seed)
                     ICA.fit(x_train)
                     x_train = ICA.transform(x_train)
                     x_test = ICA.transform(x_test)
+                    save_artifact(ICA, "ica", save_folder, uid, ica_method, None, n_run)
 
                 psd = get_PSD(
-                    x_train,
-                    sfreq=train_epochs.info["sfreq"],
-                    len_size=len_size
+                    x_train, channels=train_epochs.ch_names, sfreq=train_epochs.info["sfreq"], len_size=len_size
                 )
 
                 # Feature extraction
@@ -216,30 +234,36 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
                 psd.fit(x_train)
                 x_train = psd.transform(x_train)
                 x_test = psd.transform(x_test)
+
+                features_names = x_train.columns.tolist()
                 psd_end = time.time()
 
                 # Feature scaling
                 scaling = make_pipeline(
-                    ConcatenateChannelsPSD(),
                     StandardScaler(),
                 )
                 x_train = scaling.fit_transform(x_train, y_train)
                 x_test = scaling.transform(x_test)
 
+                x_train = pd.DataFrame(x_train, columns=features_names)
+                x_test = pd.DataFrame(x_test, columns=features_names)
+
                 # Feature selection
                 pp_start = time.time()
                 SFS = SequentialFeatureSelector(
                     LogisticRegression(),
-                    direction='forward',
+                    direction="forward",
                     cv=4,
                     scoring=make_scorer(cohen_kappa_score, greater_is_better=True),
                     n_features_to_select="auto",
-                    tol=0.01
+                    tol=0.01,
                 )
-                preprocessing = make_pipeline(SFS)
-                preprocessing.fit(x_train, y_train)
-                x_train = preprocessing.transform(x_train)
-                x_test = preprocessing.transform(x_test)
+
+                SFS.fit(x_train, y_train)
+
+                save_artifact(SFS, "sfs", save_folder, uid, ica_method, None, n_run)
+                x_train = SFS.transform(x_train)
+                x_test = SFS.transform(x_test)
                 pp_end = time.time()
 
             for clf_method in clf_methods:
@@ -250,17 +274,27 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
 
                 print(
                     "[{}|{} : {}/{}]{}({})".format(
-                        "{}{}{}".format(ica_method_color_dict[ica_method], ica_method, Style.RESET_ALL),
-                        "{}{}{}".format(clf_method_color_dict[clf_method], clf_method, Style.RESET_ALL),
+                        "{}{}{}".format(
+                            ica_method_color_dict[ica_method],
+                            ica_method,
+                            Style.RESET_ALL,
+                        ),
+                        "{}{}{}".format(
+                            clf_method_color_dict[clf_method],
+                            clf_method,
+                            Style.RESET_ALL,
+                        ),
                         n_run + 1,
                         n_runs,
                         f" {Fore.RED}-NEW-{Style.RESET_ALL} " if refit_ica else "",
-                        "ND" if is_stochastic(ica_method, clf_method) else "D"
+                        "ND" if is_stochastic(ica_method, clf_method) else "D",
                     ),
-                    end=""
+                    end="",
                 )
 
-                classifier, clf_param_grid = get_classifier(clf_method, random_state=run_seed)
+                classifier, clf_param_grid = get_classifier(
+                    clf_method, random_state=run_seed
+                )
 
                 # Classifier hyperparam opt
                 gs_cv = GridSearchCV(
@@ -270,8 +304,8 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
                     scoring=make_scorer(cohen_kappa_score, greater_is_better=True),
                     error_score=-1,
                     refit=True,
-                    n_jobs=3,
-                    verbose=0
+                    n_jobs=os.cpu_count() - 1,
+                    verbose=0,
                 )
                 fit_start = time.time()
                 gs_cv.fit(x_train, y_train)
@@ -284,46 +318,105 @@ def run(filepath, ica_methods=None, clf_methods=None, channels=None, n_runs=10, 
                         n_run,
                         ica_method,
                         clf_method,
-                        filepath.name.split(".")[0],
+                        uid,
                         accuracy_score(y_test, pred),
                         balanced_accuracy_score(y_test, pred),
                         cohen_kappa_score(y_test, pred),
                         fit_end - fit_start,
                         pp_end - pp_start,
-                        str({**gs_cv.best_params_, "selected_features": list(SFS.support_.astype(int))})
+                        str(
+                            {
+                                **gs_cv.best_params_,
+                                "selected_features": SFS.feature_names_in_.tolist(),
+                            }
+                        ),
                     ]
                 )
                 n_selected_features = SFS.support_.sum()
-                print(", Took {:.2f}s/{:.2f}s/{:.2f}s, selected {}".format(psd_end - psd_start, pp_end - pp_start, fit_end - fit_start, n_selected_features))
-                print("\tKappa", "{:.3f}".format(results[-1][-4]))
+                print(
+                    ", Took {:.2f}s/{:.2f}s/{:.2f}s, selected {}".format(
+                        psd_end - psd_start,
+                        pp_end - pp_start,
+                        fit_end - fit_start,
+                        n_selected_features,
+                    )
+                )
+                print(
+                    "\tKappa", "{:.3f}".format(cohen_kappa_score(y_test, pred))
+                )
 
-    columns = ["run", "algorithm", "classifier", "uid", "acc", "bas", "kappa", "clf_fit_time", "preprocess_fit_time", "hyperparameters"]
+    columns = [
+        "run",
+        "algorithm",
+        "classifier",
+        "uid",
+        "acc",
+        "bas",
+        "kappa",
+        "clf_fit_time",
+        "preprocess_fit_time",
+        "selected_features"
+    ]
     results_df = pd.DataFrame(results, columns=columns)
     return results_df
 
 
 if __name__ == "__main__":
-    TEST_LABEL_FOLDER = Path("/home/paulo/Documents/datasets/BCI_Comp_IV_2a/true_labels/")
-    root = Path("/home/paulo/Documents/datasets/BCI_Comp_IV_2a/gdf/")
-    selected_channels = ["EEG-Fz", "EEG-C3", "EEG-C4", "EEG-Cz"]
-    clf_methods = ["mlp", "random_forest", "extra_trees", "gaussian_nb", "lda", "svm_sigmoid", "svm_poly", "svm_linear", "svm_rbf", "logistic_l2", "logistic_l1", "logistic"]
 
-    filepaths = list(sorted(root.glob("A*T.gdf")))
-    N_RUNS = 25
+    bci_dataset_folderpath = Path("/home/paulo/Documents/datasets/BCI_Comp_IV_2a/gdf/")
+    bci_test_dataset_folderpath = Path(
+        "/home/paulo/Documents/datasets/BCI_Comp_IV_2a/true_labels/"
+    )
+    dataset = BCI_IV_Comp_Dataset(
+        bci_dataset_folderpath, test_folder=bci_test_dataset_folderpath
+    )
 
-    deterministic_methods = ["none", "orica 0", "orica 1", "ext_infomax", "infomax", "sobi", "jade", "picard", "fastica", "picard_o", "whitening", "pca"]
+    clf_methods = [
+        #"mlp",
+        "random_forest",
+        "extra_trees",
+        "gaussian_nb",
+        "lda",
+        "svm_sigmoid",
+        "svm_poly",
+        "svm_linear",
+        "svm_rbf",
+        "logistic_l2",
+        "logistic_l1",
+        "logistic",
+    ]
+
+    N_RUNS = 2
+
+    ica_methods = [
+        "none",
+        "orica 0",
+        #"orica 1",
+        "ext_infomax",
+        "infomax",
+        "sobi",
+        "jade",
+        "picard",
+        "fastica",
+        "picard_o",
+        #"whitening",
+        #"pca",
+    ]
 
     deterministic_results = dict()
-
-    for filepath in filepaths:
-        print(filepath.name)
-        subject_results_dict = run(filepath, ica_methods=deterministic_methods, clf_methods=clf_methods, n_runs=N_RUNS)
-        deterministic_results[filepath.name] = subject_results_dict
-
-        results_df = pd.concat(
-            list(deterministic_results.values()),
-            axis=0
+    save_folder = Path("results")
+    for uid in dataset.list_uids()[:1]:
+        subject_results_dict = run(
+            dataset,
+            uid,
+            save_folder,
+            ica_methods=ica_methods[:2],
+            clf_methods=clf_methods[:1],
+            n_runs=N_RUNS,
         )
+        deterministic_results[uid] = subject_results_dict
+
+        results_df = pd.concat(list(deterministic_results.values()), axis=0)
         results_df = repeat_deterministic(results_df, times=N_RUNS)
 
         results_df.rename(
@@ -332,9 +425,8 @@ if __name__ == "__main__":
                 "acc": "Acc.",
                 "bas": "Bal. Acc.",
             },
-            inplace=True
+            inplace=True,
         )
         results_df.algorithm = results_df.algorithm.apply(alg_rename)
-        results_df.uid = results_df.uid.apply(extract_subject_id)
 
         results_df.to_csv("results.csv", index=False)
--- experiments/issue_32/utils.py
+++ experiments/issue_32/utils.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 import mne
 import numpy as np
-import pandas as pd
-from ica_benchmark.io.load import BCI_IV_Comp_Dataset
 from mne.decoding import CSP
 from mne.time_frequency import psd_array_welch, psd_welch
-from numpy.linalg import eig
 from sklearn.base import BaseEstimator
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
@@ -14,10 +11,22 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-
+import pandas as pd
 
 TEST_LABEL_FOLDER = Path("/home/paulo/Documents/datasets/BCI_Comp_IV_2a/true_labels/")
-ALL_CLF_METHODS = ["lda", "svm_rbf", "svm_linear", "svm_poly", "svm_sigmoid", "knn", "random_forest", "extra_trees", "gaussian_nb", "mlp", "logistic"]
+ALL_CLF_METHODS = [
+    "lda",
+    "svm_rbf",
+    "svm_linear",
+    "svm_poly",
+    "svm_sigmoid",
+    "knn",
+    "random_forest",
+    "extra_trees",
+    "gaussian_nb",
+    "mlp",
+    "logistic",
+]
 
 
 def cue_name(cue):
@@ -48,8 +57,9 @@ class PSD(BaseEstimator):
         "beta": (13, 25),
     }
 
-    def __init__(self, **kwargs):
+    def __init__(self, channels, **kwargs):
         super(PSD).__init__()
+        self.channels = channels
         self.kwargs = kwargs
 
     def set_params(self, **params):
@@ -61,16 +71,17 @@ class PSD(BaseEstimator):
         return self.kwargs
 
     def fit(self, x, y=None):
+        self.feature_names = [f"{channel}_{band}" for channel in self.channels for band in self.BANDS_DICT]
         return self
 
     def transform(self, x, y=None):
         if isinstance(x, list):
             x = mne.concatenate_epochs(x)
-            psds, freqs = psd_welch(x, ** self.kwargs)
+            psds, freqs = psd_welch(x, **self.kwargs)
         if isinstance(x, mne.Epochs):
-            psds, freqs = psd_welch(x, ** self.kwargs)
+            psds, freqs = psd_welch(x, **self.kwargs)
         if isinstance(x, np.ndarray):
-            psds, freqs = psd_array_welch(x, ** self.kwargs)
+            psds, freqs = psd_array_welch(x, **self.kwargs)
         if ("average" in self.kwargs) and (self.kwargs["average"] is None):
             psds = psds.sum(axis=3)
         self.freqs = freqs
@@ -78,65 +89,24 @@ class PSD(BaseEstimator):
         band_spectras = list()
         for band, (lfreq, hfreq) in self.BANDS_DICT.items():
             band_spectra = psds[:, :, (freqs >= lfreq) & (freqs < hfreq)]
-            band_spectras.append(
-                band_spectra.sum(axis=2, keepdims=True)
-            )
+            band_spectras.append(band_spectra.sum(axis=2, keepdims=True))
 
         band_spectras = np.concatenate(band_spectras, axis=2)
+        features = pd.DataFrame(
+            band_spectras.reshape(len(band_spectras), -1),
+            columns=self.feature_names
+        )
 
-        return band_spectras
-
-
-# LOADING
-def preprocess_epochs(epochs, car_filter=True):
-
-    epochs = epochs.copy()
-    epochs.load_data()
-
-    if car_filter:
-        epochs.set_eeg_reference("average")
-
-    return epochs
+        return features
 
 
-def load_subject_epochs(root, subject_number, test_label_folder=TEST_LABEL_FOLDER):
-    train_file_path = root / "A{}T.gdf".format(str(subject_number).rjust(2, "0"))
-    test_file_path = root / "A{}E.gdf".format(str(subject_number).rjust(2, "0"))
-    test_label_file_path = test_label_folder / "A{}E.csv".format(str(subject_number).rjust(2, "0"))
-    train_epochs = BCI_IV_Comp_Dataset.load_dataset(
-        [train_file_path],
-        reject=False,
-        as_epochs=True,
-        concatenate=False,
-        drop_bad=False,
-        return_metadata=False,
-        tmin=0.,
-        tmax=4.,
-    )[0]
-    test_epochs = BCI_IV_Comp_Dataset.load_dataset(
-        [test_file_path],
-        reject=False,
-        as_epochs=True,
-        concatenate=False,
-        drop_bad=False,
-        return_metadata=False,
-        tmin=0.,
-        # The last timestamp does not exist, so MNE will ignore the last epoch because it will not end in 6s
-        # So here we use 5.5 seconds because there will always be 5.5 seconds after a event
-        tmax=4.,
-        has_labels=False
-    )[0]
-
-    train_epochs = preprocess_epochs(train_epochs, car_filter=False)
-    test_epochs = preprocess_epochs(test_epochs, car_filter=False)
-
-    train_labels = train_epochs.events[:, 2]
-
-    test_labels = pd.read_csv(test_label_file_path, header=None).to_numpy().flatten() - 1
-    assert len(test_labels) == len(test_epochs), "{} epochs | {} labels".format(len(test_epochs), len(test_labels))
-
-    test_epochs.events[:, 2] = test_labels
-    test_labels = test_epochs.events[:, 2]
+def load_subject_epochs(dataset, uid, load_kwargs):
+    train_epochs, train_labels = dataset.load_subject(
+        uid, **load_kwargs, run=1, session=1
+    )
+    test_epochs, test_labels = dataset.load_subject(
+        uid, **load_kwargs, run=1, session=2
+    )
 
     return (train_epochs, train_labels), (test_epochs, test_labels)
 
@@ -147,9 +117,7 @@ def get_corr_features(x):
     for i in range(len(x)):
         corr_matrix = np.corrcoef(x[i])
         idx1, idx2 = np.triu_indices(corr_matrix.shape[0], 1)
-        features.append(
-            corr_matrix[idx1, idx2].reshape(1, -1)
-        )
+        features.append(corr_matrix[idx1, idx2].reshape(1, -1))
     return np.vstack(features)
 
 
@@ -187,7 +155,7 @@ def alg_rename(alg_name):
         "Orica (1)": "ORICA (1)",
         "jade": "JADE",
         "sobi": "SOBI",
-        "fastica": "FastICA"
+        "fastica": "FastICA",
     }
     return alg_rename_dict.get(alg_name, alg_name)
 
@@ -196,70 +164,52 @@ def get_n(x):
     return eval(str(x))["sequentialfeatureselector__n_features_to_select"]
 
 
-def extract_subject_id(s):
-    return int(s[1:-1])
-
-
 def get_classifier(clf_method, random_state=1):
     if clf_method == "lda":
         clf = LinearDiscriminantAnalysis(n_components=1, solver="svd")
-        param_grid = dict(
-            n_components=[1, 2, 3]
-        )
+        param_grid = dict(n_components=[1, 2, 3])
     elif clf_method == "svm_rbf":
         clf = SVC(kernel="rbf", random_state=random_state)
-        param_grid = dict(
-            C=np.logspace(-1, 3, 6),
-            gamma=np.logspace(-1, -3, 5)
-        )
+        param_grid = dict(C=np.logspace(-1, 3, 6), gamma=np.logspace(-1, -3, 5))
     elif clf_method == "svm_linear":
         clf = SVC(kernel="linear", random_state=random_state)
-        param_grid = dict(
-            C=np.logspace(-2, 3, 10)
-        )
+        param_grid = dict(C=np.logspace(-2, 3, 10))
     elif clf_method == "svm_poly":
         clf = SVC(kernel="poly", random_state=random_state)
         param_grid = dict(
-            C=np.logspace(-1, 3, 6),
-            gamma=np.logspace(-1, -3, 4),
-            degree=[2, 3]
+            C=np.logspace(-1, 3, 6), gamma=np.logspace(-1, -3, 4), degree=[2, 3]
         )
     elif clf_method == "svm_sigmoid":
         clf = SVC(kernel="sigmoid", C=1, random_state=random_state)
-        param_grid = dict(
-            C=np.logspace(-1, 3, 6),
-            gamma=np.logspace(-1, -3, 6)
-        )
+        param_grid = dict(C=np.logspace(-1, 3, 6), gamma=np.logspace(-1, -3, 6))
     elif clf_method == "knn":
         clf = KNeighborsClassifier()
         param_grid = dict(
-            n_neighbors=[2, 4, 6, 8, 10],
-            weights=["uniform", "distance11"]
+            n_neighbors=[2, 4, 6, 8, 10], weights=["uniform", "distance11"]
         )
     elif clf_method == "random_forest":
         clf = RandomForestClassifier(random_state=random_state)
         param_grid = dict(
             n_estimators=[10, 15, 25, 40],
             max_features=["sqrt", "log2"],
-            max_depth=[2, 3]
+            max_depth=[2, 3],
         )
     elif clf_method == "extra_trees":
         clf = ExtraTreesClassifier(random_state=random_state)
-        param_grid = dict(
-            n_estimators=[10, 20, 30, 40],
-            max_depth=[2, 3, 5]
-        )
+        param_grid = dict(n_estimators=[10, 20, 30, 40], max_depth=[2, 3, 5])
     elif clf_method == "gaussian_nb":
         clf = GaussianNB()
         param_grid = dict()
     elif clf_method == "mlp":
-        clf = MLPClassifier(random_state=random_state, max_iter=5000, validation_fraction=0.25)
+        clf = MLPClassifier(
+            random_state=random_state, max_iter=5000, validation_fraction=0.25
+        )
         param_grid = dict(
             hidden_layer_sizes=[
                 (),
-                (10, ),
-                (6, ),
-                (4, ),
+                (10,),
+                (6,),
+                (4,),
                 (10, 6),
                 (6, 4),
                 (4, 4),
@@ -270,9 +220,7 @@ def get_classifier(clf_method, random_state=1):
     elif clf_method == "logistic_l2":
         clf = LogisticRegression(random_state=random_state, max_iter=3000)
         param_grid = dict(
-            C=np.logspace(-2, 3, 20),
-            penalty=["l2"],
-            fit_intercept=[True]
+            C=np.logspace(-2, 3, 20), penalty=["l2"], fit_intercept=[True]
         )
     elif clf_method == "logistic_l1":
         clf = LogisticRegression(random_state=random_state, max_iter=3000)
@@ -280,14 +228,11 @@ def get_classifier(clf_method, random_state=1):
             C=np.logspace(-2, 3, 20),
             penalty=["l1"],
             solver=["liblinear"],
-            fit_intercept=[True]
+            fit_intercept=[True],
         )
     elif clf_method == "logistic":
         clf = LogisticRegression(random_state=random_state, max_iter=3000)
-        param_grid = dict(
-            penalty=["none"],
-            fit_intercept=[True]
-        )
+        param_grid = dict(penalty=["none"], fit_intercept=[True])
     else:
         raise Exception("Unknown classifier method: {}".format(clf_method))
 
